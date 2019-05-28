8b7dbcf972b0083130565b22e66a6894946fe49e
esther <maengester@172.20.10.2>
2019-05-28 22:02:11 +0900

    다시 셋팅

--- src/main/java/com/esther/controller/ReserveController.java
+++ src/main/java/com/esther/controller/ReserveController.java
@@ -1,10 +1,5 @@
 package com.esther.controller;
 
-import java.sql.Date;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -16,24 +11,25 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.esther.dnd.DateFormating;
 import com.esther.model.ReservationVO;
-import com.esther.service.ReservationService;
 
 @Controller
 public class ReserveController {
 
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	@Inject
+	DateFormating dateFormating;
 	
 	@RequestMapping(value = "/reserve", method = RequestMethod.GET)
 	public ModelAndView reserve(Locale locale, Model model) {
 		logger.info("갤러리 페이지 >>>>>>>>>>>>>>>>>>>" );
+		System.out.println("머야야야야야");
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("/reserve");
  
@@ -51,19 +47,9 @@ public class ReserveController {
 			vo.setReserv_phone((String)formData.get("phone"));
 			vo.setReserv_email((String)formData.get("email"));
 			vo.setReserv_persons(Integer.valueOf((String) formData.get("persons")));
-			
-			
-		/*
-		 * SimpleDateFormat sdf = new sim
-		 * 
-		 * Date date=Date.valueOf(); Date dd = vo.setReserv_date(date);
-		 * System.out.println(dd);
-		 */
-		/*
-		 * DateFormat df = new SimpleDateFormat("MM-dd-yyyy"); String temp
-		 * =(String)formData.get("date"); String fm = df.format(temp); Date date =
-		 * Date.valueOf(fm);
-		 */
+			dateFormating.transformDate((String)formData.get("date"));
+		
+		
 
 
 			
