b5f890ebfe2a1ece7760f4d70783afdeb6c9687a
esther <maengester@172.20.10.2>
2019-06-05 00:52:03 +0900

    week 로직 완료 - jsp 에서 어떤식으로 뿌려줘야하는가. 오류 발견 : 데이터는맞게 들어가는거 같은데 .. date, time이 디비에서 바뀜

--- src/main/java/com/esther/controller/CalendarController.java
+++ src/main/java/com/esther/controller/CalendarController.java
@@ -1,6 +1,7 @@
 package com.esther.controller;
 
 import java.sql.Date;
+
 import java.text.Format;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -22,9 +23,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.esther.dnd.GetAWeek;
 import com.esther.model.ReservationVO;
 import com.esther.service.ReservationService;
+import com.esther.util.GetAWeek;
 
 @Controller
 public class CalendarController {
@@ -43,7 +44,6 @@ public class CalendarController {
 				System.out.println(dayOfWeek); 
 				Date localDate = Date.valueOf(ld); // 비교를 위한 형변환
 				String todayDate = localDate.toString();
-				
 		//***************************************************
 		//전체 예약 데이터 가져오기
 				List<ReservationVO> data = null;
@@ -58,27 +58,34 @@ public class CalendarController {
 				List<ReservationVO> daily = new ArrayList<ReservationVO>();
 		  for (ReservationVO vo : data) { 
 			  String reservDate = vo.getReserv_date().toString();
-			 if(reservDate.equals( todayDate)) {
+			 if(reservDate.equals(todayDate)) {
 				 boolean v = daily.add(vo);
-				 System.out.println(vo.toString()+"v:"+v);
+				 System.out.println("daily data : "+vo.toString());
 			 }else {
-				 logger.info("calendar 페이지 - - Daily 데이터 오류" );
+				 System.out.println("didididididi"+reservDate);
+				 System.out.println("calendar 페이지 - - Daily 데이터 오류" );
 			 }
 		  
 	}
 		//***************************************************
-		//week - 해당일을 기준으로 
-		  GetAWeek getAweek = new GetAWeek();
+		//week - 해당일을 기준으로 그 주간 날짜들 가져오기 (monday ~ sunday)
+		List<String> sevenDays = new ArrayList<String>();
+	   GetAWeek getAweek = new GetAWeek();
      	String monday = getAweek.getCurMonday();
      	String sunday = getAweek.getCurSunday();
-     	
-		  
-		//현재 날짜 기준으로 일요일 구하기 
-
-
-
-		
-		
+     	sevenDays =  getAweek.getSevenDates(monday, sunday);
+     	List<ReservationVO> week = new ArrayList<ReservationVO>();
+     	for (ReservationVO vo : data) { 
+			  String reservDate = vo.getReserv_date().toString();
+			for (String str : sevenDays) {
+				if(reservDate.equals( str)) {
+					boolean v = week.add(vo);
+					System.out.println(vo.toString()+"v:"+v);
+				}else {
+					logger.info("calendar 페이지 - - Week 데이터 오류" );
+				}
+			}
+     	}
 		//***************************************************
 		//month
 				
@@ -90,7 +97,10 @@ public class CalendarController {
 		  
 		  
 		  
-		model.addAttribute("daily", daily);
+     	model.addAttribute("daily", daily);
+     	model.addAttribute("weekEndDate", monday);
+     	model.addAttribute("weekStartDate", sunday);
+     	model.addAttribute("week", week);
 		model.addAttribute("localDate", localDate);
         model.addAttribute("data", data);
 		
--- src/main/java/com/esther/util/GetAWeek.java
+++ src/main/java/com/esther/util/GetAWeek.java
@@ -1,16 +1,15 @@
 package com.esther.util;
 
-import java.awt.List;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class GetAWeek {
-
-	
-	//현재 날짜 주차
+	//현재 날짜 몇 주차
 	 	public static String getWeek(){
 
 	 		Calendar c = Calendar.getInstance();
@@ -33,71 +32,62 @@ public class GetAWeek {
 	 		c.set(Calendar.WEEK_OF_MONTH,w);
 	 		c.set(Calendar.DAY_OF_WEEK,Calendar.SUNDAY);
 	 		c.add(c.DATE,7);
-
 	 		return formatter.format(c.getTime());
 
 	 	}
 
 	 	//현재 날짜 일요일
-
 	 	public static String getCurSunday(){
-
 	 		java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat("yyyy-MM-dd");
 	 		Calendar c = Calendar.getInstance();
-
 	 		c.set(Calendar.DAY_OF_WEEK,Calendar.SUNDAY);
 	 		c.add(c.DATE,7);
 	 		return formatter.format(c.getTime());
-
 	 	}
-	 	
 	 	//현재 날짜 월요일 
 	 	public static String getCurMonday(){
 	 		java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat("yyyy-MM-dd");
 	 		Calendar c = Calendar.getInstance();
 	 		c.set(Calendar.DAY_OF_WEEK,Calendar.MONDAY);
 	 		return formatter.format(c.getTime());
-
 	 	}
 	 	
 	 	//================================
 	 	public static List getSevenDates(String startDate,String endDate){
-	 	List list = null;
-		Date date = new Date();
-		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+	 		Date date = new Date();
+			String s1=startDate;
+			String s2=endDate;
+			DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 
-		//Date타입으로 변경
+			//Date타입으로 변경
+			Date d1 = null;
+			Date d2 = null;
+			try {
+				d1 = df.parse( s1 );
+				d2 = df.parse( s2 );
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
-		Date d1 = null;
-		Date d2 = null;
-		
-		try {
-			d1 = df.parse( startDate );
-			d2 = df.parse( endDate );
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			Calendar c1 = Calendar.getInstance();
+			Calendar c2 = Calendar.getInstance();
 
-		Calendar c1 = Calendar.getInstance();
-		Calendar c2 = Calendar.getInstance();
+			//Calendar 타입으로 변경 add()메소드로 1일씩 추가해 주기위해 변경
+			c1.setTime( d1 );
+			c2.setTime( d2 );
 
-		//Calendar 타입으로 변경 add()메소드로 1일씩 추가해 주기위해 변경
-		c1.setTime( d1 );
-		c2.setTime( d2 );
+			SimpleDateFormat sdf =new SimpleDateFormat("yyyy-MM-dd");
+			List<String> list = new ArrayList<String>();
 
-		//시작날짜와 끝 날짜를 비교해, 시작날짜가 작거나 같은 경우 출력
-
-		while( c1.compareTo( c2 ) !=1 ){
-
-		//출력
-		System.out.println("//////////////////////////");
-		System.out.printf("%tF\n",c1.getTime());
-
-		//시작날짜 + 1 일
-		c1.add(Calendar.DATE, 1);
-		}
-		return list;
-
-	 	}
+			while( c1.compareTo( c2 ) !=1 ){
+				//출력
+				String strDate = sdf.format(c1.getTime());
+				list.add(strDate);
+				//시작날짜 + 1 일
+				  c1.add(Calendar.DATE, 1);
+				}
+			System.out.println("List 7days : "+ list);
+			return list;
+			}
 }
